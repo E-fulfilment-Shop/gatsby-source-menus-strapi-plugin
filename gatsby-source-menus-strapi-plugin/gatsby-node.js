1de25f4f700d06bca4b6ad0ea824577c2c56624f
Siraje Amarniss <siraje@e-fulfilmentshop.nl>
2023-11-28 01:12:37 +0100

    Add access token & auto-indent

--- gatsby-source-menus-strapi-plugin/gatsby-node.js
+++ gatsby-source-menus-strapi-plugin/gatsby-node.js
@@ -2,47 +2,53 @@ const axios = require("axios");
 
 const MENU_NODE_TYPE = "StrapiMenus";
 
-const fetchData = async (url) => await axios.get(url);
+const fetchData = async (url, headers) => await axios.get(url, headers);
 
 exports.sourceNodes = async (
-  { actions, createContentDigest, createNodeId },
-  { apiURL, menusEndpoint, nested = false, menuID = "" }
+    { actions, createContentDigest, createNodeId },
+    { apiURL, accessToken, menusEndpoint, nested = false, menuID = "" }
 ) => {
-  if (!apiURL)
-    return reporter.panic(
-      "Gatsby-source-menus-strapi-plugin: Please provide your strapi apiURL endpoint"
+    if (!apiURL)
+        return reporter.panic(
+            "Gatsby-source-menus-strapi-plugin: Please provide your strapi apiURL endpoint"
+        );
+
+    const { createNode } = actions;
+
+    const nestedParam = nested
+        ? `${menusEndpoint}/${menuID}?nested&populate=*`
+        : menusEndpoint;
+    const fetch_url = `${apiURL}/api/${nestedParam}`;
+
+    let headers = {};
+
+    if (accessToken) {
+        headers.authorization = `Bearer ${accessToken}`;
+    }
+
+    const { data: response } = await fetchData(fetch_url, headers).catch((error) => {
+        throw new Error(
+            `🚫🚫🚫🙅‍♀️🙅‍♀️🙅‍♀️ Error fetching data from ${fetch_url} - ${error} 🚫🚫🚫🙅‍♀️🙅‍♀️🙅‍♀️`
+        );
+    });
+
+    const arrayOfItems = menuID
+        ? response.data.attributes.items.data
+        : response.data;
+
+    arrayOfItems.forEach((item) =>
+        createNode({
+            ...item,
+            id: createNodeId(`${MENU_NODE_TYPE}-${item.id}`),
+            parent: null,
+            children: [],
+            internal: {
+                type: MENU_NODE_TYPE,
+                content: JSON.stringify(item),
+                contentDigest: createContentDigest(item),
+            },
+        })
     );
 
-  const { createNode } = actions;
-
-  const nestedParam = nested
-    ? `${menusEndpoint}/${menuID}?nested&populate=*`
-    : menusEndpoint;
-  const fetch_url = `${apiURL}/api/${nestedParam}`;
-
-  const { data: response } = await fetchData(fetch_url).catch((error) => {
-    throw new Error(
-      `🚫🚫🚫🙅‍♀️🙅‍♀️🙅‍♀️ Error fetching data from ${fetch_url} - ${error} 🚫🚫🚫🙅‍♀️🙅‍♀️🙅‍♀️`
-    );
-  });
-
-  const arrayOfItems = menuID
-    ? response.data.attributes.items.data
-    : response.data;
-
-  arrayOfItems.forEach((item) =>
-    createNode({
-      ...item,
-      id: createNodeId(`${MENU_NODE_TYPE}-${item.id}`),
-      parent: null,
-      children: [],
-      internal: {
-        type: MENU_NODE_TYPE,
-        content: JSON.stringify(item),
-        contentDigest: createContentDigest(item),
-      },
-    })
-  );
-
-  return;
+    return;
 };
